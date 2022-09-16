d99597d86391445700bb6737f2d0b649407d0f14
ram-xkr <ram-xkr@mail.ru>
2022-09-16 20:54:45 +0300

    ещё один вариант с треугольником

---
+++ Magic_out_of_classes/Program.cs
@@ -0,0 +1,24 @@
+﻿int[,] v = {
+    {40, 1},
+    {1, 30},
+    {80, 30}
+};
+int x = v[0, 0], y = v[0, 1];
+int count = 2000;
+
+while(true)
+{
+    Console.Clear();
+    for (int i = 1; i < count; i++)
+    {
+        int what = new Random().Next(0, 3);
+        x = (x + v[what, 0]) / 2;
+        y = (y + v[what, 1]) / 2;
+
+        Console.SetCursorPosition(x, y);
+        Console.WriteLine(".");
+    }
+    Thread.Sleep(10);
+    v[1, 0]++;
+    v[2, 0]--;
+}
