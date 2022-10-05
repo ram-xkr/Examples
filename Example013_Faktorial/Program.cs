6980b6444161531a1cfa29e6bf1ce9ca339a0582
ram-xkr <ram-xkr@mail.ru>
2022-10-05 18:43:50 +0300

    Example 013 Faktorial

---
+++ Example013_Faktorial/Program.cs
@@ -0,0 +1,17 @@
+﻿for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}"); // 1*2*3 = 6
+}
+
+
+
+
+
+
+double Factorial (int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if (n == 1) return 1;
+    else return n * Factorial(n-1);
+}
